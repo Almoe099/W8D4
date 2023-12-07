d6ef37d2ece7c930b8537a7be32468abefade157
csmanel <csmanel@gmail.com>
2023-12-07 14:53:25 -0500

    mybind complete

--- practice-for-ch-js-argument-exercises-long-practice-main/README.md
+++ practice-for-ch-js-argument-exercises-long-practice-main/README.md
@@ -6,11 +6,11 @@ These exercises will give you practice handling function arguments.
 
 By the end of this practice, you should be able to
 
-* Use `arguments` and the `...` rest operator
-* Set the context of a function in multiple ways
-* Write currying functions
-  * Explain how currying works
-  * Combine arguments from different calls of a curried function
+- Use `arguments` and the `...` rest operator
+- Set the context of a function in multiple ways
+- Write currying functions
+  - Explain how currying works
+  - Combine arguments from different calls of a curried function
 
 ## sum
 
@@ -49,31 +49,31 @@ class Dog {
   }
 }
 
-const markov = new Cat("Markov");
-const pavlov = new Dog("Pavlov");
+const markov = new Cat('Markov');
+const pavlov = new Dog('Pavlov');
 
-markov.says("meow", "Ned");
+markov.says('meow', 'Ned');
 // Markov says meow to Ned!
 // true
 
 // bind time args are "meow" and "Kush", no call time args
-markov.says.myBind(pavlov, "meow", "Kush")();
+markov.says.myBind(pavlov, 'meow', 'Kush')();
 // Pavlov says meow to Kush!
 // true
 
 // no bind time args (other than context), call time args are "meow" and "a tree"
-markov.says.myBind(pavlov)("meow", "a tree");
+markov.says.myBind(pavlov)('meow', 'a tree');
 // Pavlov says meow to a tree!
 // true
 
 // bind time arg is "meow", call time arg is "Markov"
-markov.says.myBind(pavlov, "meow")("Markov");
+markov.says.myBind(pavlov, 'meow')('Markov');
 // Pavlov says meow to Markov!
 // true
 
 // no bind time args (other than context), call time args are "meow" and "me"
 const notMarkovSays = markov.says.myBind(pavlov);
-notMarkovSays("meow", "me");
+notMarkovSays('meow', 'me');
 // Pavlov says meow to me!
 // true
 ```
--- practice-for-ch-js-argument-exercises-long-practice-main/sum.js
+++ practice-for-ch-js-argument-exercises-long-practice-main/sum.js
@@ -6,13 +6,74 @@
 //     return total;
 // }
 
-function sum(...args){
-    let total = 0
-    for (let i = 0; i < args.length; i++) {
-        total += args[i];
-    }
-    return total;
+// function sum(...args){
+//     let total = 0
+//     for (let i = 0; i < args.length; i++) {
+//         total += args[i];
+//     }
+//     return total;
+// }
+
+// console.log(sum(1, 2, 3, 4) === 10)
+// console.log(sum(1, 2, 3, 4, 5) === 15)
+
+// Function.prototype.myBind = function (ctx) {
+//   let that = this;
+//   const args = Array.from(arguments).slice(1);
+//   return function () {
+//     const callArgs = Array.from(arguments);
+//     return that.apply(ctx, args.concat(callArgs));
+//   };
+// };
+
+Function.prototype.myBind = function (ctx, ...args) {
+  let that = this;
+  return function (...callArgs) {
+    return that.apply(ctx, args.concat(callArgs));
+  };
+};
+
+class Cat {
+  constructor(name) {
+    this.name = name;
+  }
+
+  says(sound, person) {
+    console.log(`${this.name} says ${sound} to ${person}!`);
+    return true;
+  }
 }
 
-console.log(sum(1, 2, 3, 4) === 10)
-console.log(sum(1, 2, 3, 4, 5) === 15)
+class Dog {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+const markov = new Cat('Markov');
+const pavlov = new Dog('Pavlov');
+
+markov.says('meow', 'Ned');
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind(pavlov, 'meow', 'Kush')();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind(pavlov)('meow', 'a tree');
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind(pavlov, 'meow')('Markov');
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBind(pavlov);
+notMarkovSays('meow', 'me');
+// Pavlov says meow to me!
+// true
